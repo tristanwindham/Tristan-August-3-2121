0dad507ec24944710e29fd642b9713c23fbd13e6
tristanwindham <tristanwindham.com>
2021-08-02 08:39:14 -0700

    Added mobile responsive logic and styles

--- src/components/orderBook/grid/Grid.tsx
+++ src/components/orderBook/grid/Grid.tsx
@@ -1,6 +1,8 @@
-import { selectBidData, selectAskData } from './../../../store/reducers/orderBook/orderBookReducer';
+import { selectBidData, selectAskData, selectSpread } from './../../../store/reducers/orderBook/orderBookReducer';
 import { useAppSelector } from '../../../app/hooks';
+import { useMediaQuery } from 'react-responsive';
 import numeral from 'numeral'
+import { Fragment } from 'react';
 
 interface dataObject {
     price: number,
@@ -9,47 +11,75 @@ interface dataObject {
 }
 
 const Grid: React.FC = () => {
-    const bidData: dataObject[] = useAppSelector(selectBidData);
-    const askData: dataObject[] = useAppSelector(selectAskData);
+    let bidData: dataObject[] = useAppSelector(selectBidData);
+    let askData: dataObject[] = useAppSelector(selectAskData);
+    const spread: string = useAppSelector(selectSpread);
+    const isMobile: boolean = useMediaQuery({ query: `(max-width: 900px)` });
+    const dataLength: number = isMobile ? 12 : 25;
+    
+    // Formats ask data in reverse order for mobile display.
+    let sortedAskData = [...askData.slice(0, isMobile ? 12 : 25)];
+    if (isMobile) {
+        sortedAskData = sortedAskData.reverse();
+    }
 
     return (
         <div id="grid">
             <div className='grid-half'>
-                <div className="grid-row header">
-                    <div className="grid-size buy-headerl">TOTAL</div>
-                    <div className="grid-size buy-header">SIZE</div>
-                    <div className="grid-price buy-header">PRICE</div>
-                </div>
+                    { isMobile ? 
+                        null
+                        :
+                        <div className="grid-row header">
+                            <div className="grid-size buy-header">TOTAL</div>
+                            <div className="grid-size buy-header">SIZE</div>
+                            <div className="grid-price buy-header">PRICE</div>
+                        </div>
+                    }
                 {
                     bidData.map((item: dataObject, idx: number) => {
-                        const depthIdx = bidData.length < 25 ? bidData.length - 1 : 24
+                        const depthIdx = bidData.length < dataLength ? bidData.length - 1 : dataLength - 1;
                         const depth = (item.total / bidData[depthIdx].total) * 100;
                         return <div className="grid-row" key={idx}>
-                            <div className="grid-depth buy" style={{ width: `${Math.round(depth)}%` }}></div>
-                            <div className="grid-size">{numeral(item.total).format('0,0')}</div>
-                            <div className="grid-size">{numeral(item.size).format('0,0')}</div>
-                            <div className="grid-price buy">{numeral(item.price).format('0,0.00')}</div>
+                            <div className={`grid-depth buy ${isMobile ? 'mobile' : ''}`} style={{ width: `${Math.round(depth)}%` }}></div>
+
+                            {/* Swaps the column layout if mobile screen size detected. */}
+                            { isMobile ? 
+                                <Fragment>
+                                    <div className="grid-price buy mobile">{numeral(item.price).format('0,0.00')}</div>
+                                    <div className="grid-size buy">{numeral(item.size).format('0,0')}</div>
+                                    <div className="grid-size mobile">{numeral(item.total).format('0,0')}</div>
+                                </Fragment>
+                                : 
+                                <Fragment>
+                                    <div className="grid-size">{numeral(item.total).format('0,0')}</div>
+                                    <div className="grid-size">{numeral(item.size).format('0,0')}</div>
+                                    <div className="grid-price buy">{numeral(item.price).format('0,0.00')}</div>
+                                </Fragment>
+                            }
+
                         </div>
-                    }).slice(0, 25)
+                    }).slice(0, dataLength)
                 }
             </div>
+            { isMobile ? <div id="grid-spread">{spread}</div> : null }
             <div className='grid-half'>
                 <div className="grid-row header">
-                    <div className="grid-price sell-header">PRICE</div>
+                    <div className={`grid-price sell-header ${isMobile ? 'mobile' : ''}`}>PRICE</div>
                     <div className="grid-size sell-header">SIZE</div>
-                    <div className="grid-size sell-header">TOTAL</div>
+                    <div className={`grid-size sell-header ${isMobile ? 'mobile' : ''}`}>TOTAL</div>
                 </div>
                 {
-                    askData.map((item: dataObject, idx: number) => {
-                        const depthIdx = askData.length < 25 ? askData.length - 1 : 24
-                        const depth = (item.total / askData[depthIdx].total) * 100;
+                    sortedAskData.map((item: dataObject, idx: number) => {
+                        let depthIdx = sortedAskData.length < dataLength ? sortedAskData.length - 1 : dataLength - 1;
+                        if (isMobile) depthIdx = 0
+                        const depth = (item.total / sortedAskData[depthIdx].total) * 100;
                         return <div className="grid-row" key={idx}>
                             <div className="grid-depth sell" style={{ width: `${Math.round(depth)}%` }}></div>
-                            <div className="grid-price sell">{numeral(item.price).format('0,0.00')}</div>
+                            <div className={`grid-price sell ${isMobile ? 'mobile' : ''}`}>{numeral(item.price).format('0,0.00')}</div>
                             <div className="grid-size sell">{numeral(item.size).format('0,0')}</div>
-                            <div className="grid-size sell">{numeral(item.total).format('0,0')}</div>
+                            <div className={`grid-size ${isMobile ? 'mobile' : ''}`}>{numeral(item.total).format('0,0')}</div>
                         </div>
-                    }).slice(0, 25)
+                    }).slice(0, dataLength)
                 }
             </div>
         </div>
--- src/components/orderBook/gridHeader/GridHeader.tsx
+++ src/components/orderBook/gridHeader/GridHeader.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { GroupDropDown } from "./GroupDropDown";
 import { selectMarket, selectSpread} from './../../../store/reducers/orderBook/orderBookReducer';
+import { useMediaQuery } from 'react-responsive';
 import { useAppSelector } from '../../../app/hooks';
 
 const GridHeader: React.FC = () => {
     const selectedMarket: string = useAppSelector(selectMarket);
     const spread: string = useAppSelector(selectSpread);
+    const isDesktop: boolean = useMediaQuery({ query: `(min-width: 900px)` });
 
     return (
         <div id="grid-header">
             <div id="grid-header-title">{selectedMarket}</div>
-            <div id="grid-header-spread">{spread}</div>
+            { isDesktop ? <div id="grid-header-spread">{spread}</div> : null }
             <GroupDropDown />
         </div>
     )
